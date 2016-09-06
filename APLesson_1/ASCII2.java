b5c68a98ae384a7470f45b32c423b22e6ca6817d
deepthigg <GangiredlaL1745@sduhsd.lan>
2016-09-06 11:30:32 -0700

    Added Files

---
+++ APLesson_1/ASCII2.java
@@ -0,0 +1,12 @@
+public class ASCII2
+{
+	public static void main(String[]args)
+	{
+		System.out.println("            .------.     @    @   ");
+		System.out.println("        /  .-\"-.\'.         \\ v/   " );
+		System.out.println("        l  1   '\\  \\ _      /   )   ");
+		System.out.println("      ,-\\  ._.  '    / .'       )    ");
+		System.out.println("      ____\\- _ _ _ _\\_ _ ,    ");
+	}
+}
+	
