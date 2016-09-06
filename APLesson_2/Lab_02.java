1f8b0286260ad367e9950094dafe95fddf41c0e2
deepthigg <GangiredlaL1745@sduhsd.lan>
2016-09-06 12:12:25 -0700

    add lab 2

---
+++ APLesson_2/Lab_02.java
@@ -0,0 +1,14 @@
+public class Lab_02
+{
+	public static void main(String[]args)
+	{
+		int x=6;
+		int y=5;
+		int multiplication= x * y;
+		
+		System.out.println("6 and 5 produce" +" "+ multiplication);
+	}
+	{
+		
+	}
+}
