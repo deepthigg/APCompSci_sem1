6b015afa2ce5b99067b94b284e36e86bf6d13c40
deepthigg <GangiredlaL1745@sduhsd.lan>
2016-09-08 11:41:24 -0700

    added

---
+++ APLesson_3/lesson_03.java
@@ -0,0 +1,20 @@
+import java.until.Scanner; //import Statement 
+
+public class lesson_03
+{
+	public static void main(String[]args)
+	{
+		//instantiate the new Scanner object "keyboard"
+		Scanner keyboard = new Scanner(System.in);
+		
+		//prompt for your user input
+		System.out.println("How old are you?");
+		
+		//search for next integer that teh user wnters
+		int num = keybooard.nextInt();
+		
+		//print the results...
+		System.out.println(Wow! " + num + " is " is perfect. You're pretty handsome.");
+		
+	}
+}
