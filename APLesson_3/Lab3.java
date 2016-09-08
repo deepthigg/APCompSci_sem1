ff8009413157952104a4cc714e26e8f5120997e2
deepthigg <GangiredlaL1745@sduhsd.lan>
2016-09-08 12:15:00 -0700

    added

--- APLesson_3/Lab3.java
+++ APLesson_3/Lab3.java
@@ -1,6 +1,6 @@
 import java.util.Scanner; //import Statement 
 
-public class lesson_03
+public class Lab3
 {
 	public static void main(String[]args)
 	{
@@ -8,19 +8,28 @@ public class lesson_03
 		Scanner keyboard = new Scanner(System.in);
 		
 		//prompt for your user input
+		System.out.println("What is your name?");
+		
+		String name = keyboard.next();
+		
+		//print the results...
+		System.out.println("Interesting name!");
+		
+		//prompt for your user input
 		System.out.println("How old are you?");
 		
 		//search for next integer that the user enters
 		int num = keyboard.nextInt();
-		
-		//print the results...
-		System.out.println("Wow! " + num + " is perfect. You're pretty handsome.");
+	
+		System.out.println("Nice!");
 		
 		//prompt for your user input
-		System.out.println("Who is your favorite teacher");
+		System.out.println("What do you do for fun?");
 		
-		String teacher = keyboard.next();
+		String fun = keyboard.next();
+	
+		System.out.println("That sounds like fun!");
 		
-		System.out.println("Okay! " + teacher + " is very good!"); 
 	}
+	
 }
