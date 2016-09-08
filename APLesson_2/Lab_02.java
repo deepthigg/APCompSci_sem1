6700eef60ffc5f809274c96e2ae3e7c40981795f
deepthigg <GangiredlaL1745@sduhsd.lan>
2016-09-08 11:21:35 -0700

    added

--- APLesson_2/Lab_02.java
+++ APLesson_2/Lab_02.java
@@ -7,8 +7,22 @@ public class Lab_02
 		int multiplication= x * y;
 		
 		System.out.println("6 and 5 produce" +" "+ multiplication);
-	}
-	{
 		
+
+		String a = "Deepthi Gangiredla";
+		String b = "12631 El Caminio Real";
+		String c = "San Diego";
+		String d = "92130";
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c + ","+ d);
+		
+		int w=8;
+		int l=7;
+		int h=4;
+		int Surfacearea= 2 * w * l + 2 * h * l * + 2 * h * w;
+		
+		System.out.println("The surface area of the rectangle is" +" "+ Surfacearea);
 	}
 }
