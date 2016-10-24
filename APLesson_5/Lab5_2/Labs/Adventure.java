495e822a35d56cd11fa65d293efbd9cabae0af1a
deepthigg <GangiredlaL1745@sduhsd.lan>
2016-10-24 12:18:17 -0700

    changes

--- APLesson_5/Lab5_2/Labs/Adventure.java
+++ APLesson_5/Lab5_2/Labs/Adventure.java
@@ -1,25 +1,63 @@
 import java.util.Scanner;
-public class adventure
+public class Adventure
 {
 	public static void main(String[]args)
 	{
 		Scanner kb = new Scanner(System.in);
-		System.out.println("A robbey has occured. You need to find the bad guy and save America");
+		System.out.println("A robbey has occured at a store. You need to find the bad guy and save America");
+
+		System.out.println("What wil you first do....." + 
+						     "\n 1. Confront the robber directly" +
+							 "\n 2. Go to the crime scene and get solid evidence " );					 
+		int choice= kb.nextInt();
+			 {
+				if (choice ==1)
+				{
+					System.out.println("You find the robber in an abandoned warehouse.");
+					System.out.println("What will you do next....." +
+										"\n 3.Ask the robber what his intentions were" +
+										"\n 4. Handcuff the robber");
+					int choice2= kb.nextInt();
+					
+					//print: something happens
+					//prompt for new choice
+					//user input for choice value (use nextInt())
+					
+					if (choice == 3 )
+					{
+						System.out.println("Th robber tells you his intentions and the police arrest him.YOu are a hero!");
+						
+					}
+						
+					else
+					{
+						System.out.println("The police arrive and take him for interrogation. You are a hero!");
+					}
+				}
+				else
+				{
+					System.out.println(" Yuo realize that the evidence shows that the robbery did not occur.");
+					System.out.println("What will you do next....." +
+										"\n 5.Confront the the store owner and ask him his intentions" +
+										"\n 6. Handcuff the store owner ");
+					int choice3= kb.nextInt();
+					//print: something happens
+					//prompt for new choice
+					//user input for choice value (use nextInt())
+					
+					if (choice == 5 )
+					{
+						System.out.println("The owner confesses his plot to get insurance money and the police take him in. You are a hero.");
+					}
+						
+					else
+					{
+						System.out.println("The police arrive and take him for interrogation. You are a hero!");
+					}						
+				}					
+			}
 		
-		int fadvent= kb.nextInt();
-		
-		if(choice == choice 1)
-		{System.out.println("What wil you first do....." + 
-						     "\n a. "Buy a cool costume to look badass" +
-							 "\n b. "Go to the crime scene " +
-							 "\n c. "Go home and sleep");
-							 {
-								 if (choice == a )
-									 System.out.println (" What kind of costume would you buy?");
-							 }
-		}
-							
-				
+	
 	
 	}
 }
---
+++ APLesson_5/Lab5_2/Labs/Password.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class Password
+{
+	static String username = "deepthi";
+	static String password = "deepthiisawesome";
+	static Scanner kb;
+	public static void main(String[]args)
+	{
+		kb = new Scanner(System.in);
+		passCheck();
+		
+	} 
+	public static void passCheck()
+	{
+		System.out.println("What is the username");
+		String name = kb.next();
+		System.out.println("What is the password");
+		String word = kb.next();
+
+		if ( name.equals (username) && word.equals (password))
+		{
+			System.out.println("Your access is granted");
+	
+		}
+
+		else
+		{
+			if (name.equals(username))
+				System.out.println(" Password is incorrect");
+
+			else if (word.equals (password))
+				System.out.println("Username is incorrect");
+			
+			else
+				System.out.println("Your password and username are incorrect");
+		}
+		
+	}
+	
+}
