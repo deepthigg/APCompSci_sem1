8fed1d76d8b6ef95a7ccdb8c2f935b0b2a001c71
deepthigg <GangiredlaL1745@sduhsd.lan>
2016-10-26 12:14:52 -0700

    add

---
+++ APLesson_6/labs/factorial.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class box
+{
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Please enter a number");
+		int number = kb.nextInt();
+		int factorial = 1;
+		
+	`	for(int i= i <=number; i++ )
+			{
+				factorial*= i ;
+				System.out.println("Number:" + number);
+			
+			}
+			
+		
+	}
+}
