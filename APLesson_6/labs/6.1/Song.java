a7189c0dd34c689b437dee2008b585a49776c407
deepthigg <GangiredlaL1745@sduhsd.lan>
2016-11-07 12:08:50 -0800

    add

--- APLesson_6/labs/6.1/Song.java
+++ APLesson_6/labs/6.1/Song.java
@@ -4,9 +4,19 @@ public class Song
 	public static void main(String[]args)
 	{
 		Scanner kb = new Scanner(System.in);
-	}
-	public static sing()
-	{
+		sing ("Na", 4);
+		sing ("Na", 4);
+		sing ("Hey", 3);
+		sing ("Goodbye", 2);
 		
+	}
+	
+	public static void sing(String lyric, int number)
+	{
+		for(int i =0; i <= number; i++)
+		{
+			System.out.print(lyric + " ");
+		}
+		System.out.println();
 	}
 }
