f1eaba23b51b723a6933ff92a74613405e960fc9
deepthigg <GangiredlaL1745@sduhsd.lan>
2016-11-09 12:12:48 -0800

    add

--- APLesson_8/Labs8/underscore.java
+++ APLesson_8/Labs8/underscore.java
@@ -1,22 +1,23 @@
 import java.util.Scanner;
 public class underscore
 {
+	static String sentence;
 	
 	public static void main(String[]args)
 	{
 		Scanner kb = new Scanner(System.in);
 		System.out.println("Please enter a sentence:");
-
-		System.out.println(replace(kb.nextLine()));
+		sentence= kb.nextLine();
+		System.out.println(replace(sentence));
 		
 	}
 	public static String replace(String sentence)
 	{
 		while(sentence.indexOf(" ") >= 0)
 		{
-			return sentence.substring(0,sentence.indexOf(" ")) + "_" + sentence.substring(sentence.indexOf(" "));
+			return replace(sentence.substring(0,sentence.indexOf(" ")) + "_" + sentence.substring(sentence.indexOf(" ") +1));
 		}
-		return "";
+		return sentence;
 	}
 	
 }
